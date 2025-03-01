987c1038cde8db5f2a272587e92d78b34ea87e3b
thoughtfuldarwin <chandamalavijay@gmail.com>
2025-03-01 18:48:02 +0530

    Block commit when any hook fails

--- cmd/run.go
+++ cmd/run.go
@@ -71,7 +71,7 @@ func runHooks(cmd *cobra.Command, args []string) error {
 	}
 
 	if len(activeHooks) == 0 {
-		fmt.Println("No active %s hooks found.", hookType)
+		fmt.Printf("No active %s hooks found.\n", hookType)
 		return nil
 	}
 
